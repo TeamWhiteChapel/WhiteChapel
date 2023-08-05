993774af53602bcc8cad65b2bac8e9d7e40325e8
seintcat <1x2x0x7@naver.com>
2023-08-06 01:59:40 +0900

    화면 페이드인 기능 오류 수정, 데모 씬 완성

--- WhiteChapel/Assets/Scripts/Fade/FadeUI.cs
+++ WhiteChapel/Assets/Scripts/Fade/FadeUI.cs
@@ -21,7 +21,7 @@ public class FadeUI : MonoBehaviour
 
     private void Awake()
     {
-        if (imageFade != null)
+        if (imageFade == null)
         {
             Error("Can't find fade image!");
             return;
@@ -31,7 +31,7 @@ public class FadeUI : MonoBehaviour
             Error("Can't find fade datas!");
             return;
         }
-        if (fitter != null)
+        if (fitter == null)
         {
             Error("Can't find aspect ratio fitter!");
             return;
@@ -40,9 +40,10 @@ public class FadeUI : MonoBehaviour
         fadeIndex = 0;
         imageFade.raycastTarget = false;
         imageFade.enabled = false;
+        imageFade.color = Color.white;
     }
 
-    private void FadeStart(Action action, int fadeDataIndex = -1)
+    public void FadeStart(Action action, int fadeDataIndex = -1)
     {
         // Reset
         actionInFade = action;
@@ -60,18 +61,10 @@ public class FadeUI : MonoBehaviour
             return;
         }
 
-        // Check fade in skipped
-        if (fadeDatas[fadeIndex].fadeOption == FadeOption.ClipFadeIn)
-        {
-            // Skip to fade out
-            FadeOutReset();
-        }
-
         // Fade in
         if (fadeNow != null)
         {
             StopCoroutine(fadeNow);
-            fadeNow.Reset();
         }
         if (fadeDatas[fadeIndex].sprite != null)
         {
@@ -83,13 +76,21 @@ public class FadeUI : MonoBehaviour
             imageFade.sprite = null;
         }
         fadeNow = FadeIn();
+
+        // Check fade in skipped
+        if (fadeDatas[fadeIndex].fadeOption == FadeOption.ClipFadeIn)
+        {
+            // Skip to fade out
+            FadeOutReset();
+            return;
+        }
         StartCoroutine(fadeNow);
     }
     private IEnumerator FadeIn()
     {
         fadeWait = new WaitForSeconds(fadeDatas[fadeIndex].fadeIn.fadeTime);
         fadeDatas[fadeIndex].fadeIn.fadeColor.a = 0;
-        imageFade.color = fadeDatas[fadeIndex].fadeIn.fadeColor;
+        imageFade.CrossFadeColor(fadeDatas[fadeIndex].fadeIn.fadeColor, 0, false, true);
         fadeDatas[fadeIndex].fadeIn.fadeColor.a = 1;
         imageFade.CrossFadeColor(fadeDatas[fadeIndex].fadeIn.fadeColor, fadeDatas[fadeIndex].fadeIn.fadeTime, false, true);
         yield return fadeWait;
@@ -104,6 +105,7 @@ public class FadeUI : MonoBehaviour
         // Check fade out skipped
         if (fadeDatas[fadeIndex].fadeOption == FadeOption.ClipFadeOut)
         {
+            imageFade.enabled = false;
             return;
         }
 
@@ -111,16 +113,18 @@ public class FadeUI : MonoBehaviour
         if (fadeNow != null)
         {
             StopCoroutine(fadeNow);
-            fadeNow.Reset();
         }
         fadeNow = FadeOut();
         StartCoroutine(fadeNow);
     }
     private IEnumerator FadeOut()
     {
-        fadeWait = new WaitForSeconds(fadeDatas[fadeIndex].fadeIn.fadeTime);
-        fadeDatas[fadeIndex].fadeIn.fadeColor.a = 1;
-        imageFade.color = fadeDatas[fadeIndex].fadeIn.fadeColor;
+        fadeWait = new WaitForSeconds(fadeDatas[fadeIndex].fadeOut.fadeTime);
+        if (fadeDatas[fadeIndex].fadeOption == FadeOption.ClipFadeIn)
+        {
+            fadeDatas[fadeIndex].fadeOut.fadeColor.a = 1;
+            imageFade.CrossFadeColor(fadeDatas[fadeIndex].fadeOut.fadeColor, 0, false, true);
+        }
         fadeDatas[fadeIndex].fadeOut.fadeColor.a = 0;
         imageFade.CrossFadeColor(fadeDatas[fadeIndex].fadeOut.fadeColor, fadeDatas[fadeIndex].fadeOut.fadeTime, false, true);
         yield return fadeWait;
