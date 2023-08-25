4cbe77ecec8c5b9df024b698d31788c053e98f4b
SShinMJ <winny0678@gmail.com>
2023-08-25 17:52:21 +0900

    Feat: 오브젝트 상호작용: 눈 아이콘 띄우기

---
+++ WhiteChapel/Assets/Scripts/ObjectInterationUI/CameraMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraMovement : MonoBehaviour
+{
+    float h, v, xRotate, yRotate, xRotateMove, yRotateMove;
+    public float moveSpeed = 3f;
+    public float rotateSpeed = 200f;
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(h, 0, v) * moveSpeed * Time.deltaTime;
+
+        xRotateMove = -Input.GetAxis("Mouse Y") * Time.deltaTime * rotateSpeed;
+        yRotateMove = Input.GetAxis("Mouse X") * Time.deltaTime * rotateSpeed;
+
+        yRotate = transform.eulerAngles.y + yRotateMove;
+        xRotate += xRotateMove;
+        xRotate = Mathf.Clamp(xRotate, -90, 90);
+        transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+    }
+}
---
+++ WhiteChapel/Assets/Scripts/ObjectInterationUI/ItemSelectUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSelectUI : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] Sprite idleSprite;
+    [SerializeField] Sprite selectedSprite;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnMouseOver()
+    {
+
+    }
+
+    public void OnMouseExit()
+    {
+
+    }
+}
