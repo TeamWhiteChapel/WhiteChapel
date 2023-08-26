a17534b4d87eef9005e645a415bb0b24ff320281
SShinMJ <winny0678@gmail.com>
2023-08-26 19:45:17 +0900

    Fix: Raycast 오류 수정(타겟 확인 로직 추가)

--- WhiteChapel/Assets/Scripts/ObjectInterationUI/ItemSelectUI.cs
+++ WhiteChapel/Assets/Scripts/ObjectInterationUI/ItemSelectUI.cs
@@ -1,11 +1,12 @@
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemSelectUI : MonoBehaviour
 {
-    Image image;
-    Sprite idleSprite;
-    Sprite selectedSprite;
+    [SerializeField] Image image;
+    [SerializeField] Sprite idleSprite;
+    [SerializeField] Sprite selectedSprite;
 
     LayerMask InteractionObj;
     [SerializeField] float rayMaxDistance = 0.5f;
@@ -22,10 +23,18 @@ public class ItemSelectUI : MonoBehaviour
     void Update()
     {
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo = new RaycastHit();
 
-        if (Physics.Raycast(ray, rayMaxDistance, InteractionObj))
+        if (Physics.Raycast(ray, out hitInfo, rayMaxDistance, InteractionObj))
         {
-            OnMouseOver();
+            if(hitInfo.collider.gameObject == gameObject)
+            {
+                OnMouseOver();
+            }
+            else
+            {
+                OnMouseExit();
+            }
         }
         else
         {
