a54a74d8d49a2c9525b8fbd95c5dfc5e5c7e5f4f
SShinMJ <winny0678@gmail.com>
2023-08-26 19:03:30 +0900

    Feat: 화면 따라 UI 회전, Raycating 및 마우스 위치별 이미지 전환 추가

--- WhiteChapel/Assets/Scripts/ObjectInterationUI/ItemSelectUI.cs
+++ WhiteChapel/Assets/Scripts/ObjectInterationUI/ItemSelectUI.cs
@@ -1,31 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemSelectUI : MonoBehaviour
 {
-    [SerializeField] Image image;
-    [SerializeField] Sprite idleSprite;
-    [SerializeField] Sprite selectedSprite;
+    Image image;
+    Sprite idleSprite;
+    Sprite selectedSprite;
 
-    void Start()
+    LayerMask InteractionObj;
+    [SerializeField] float rayMaxDistance = 0.5f;
+
+    private void Start()
     {
-        
+        image = transform.Find("InterationCanvas").GetComponentInChildren<Image>();
+        idleSprite = Resources.Load<Sprite>("InterationUI/Eye_Closed");
+        selectedSprite = Resources.Load<Sprite>("InterationUI/Eye_Open");
+
+        InteractionObj = LayerMask.GetMask("Interaction");
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
+        if (Physics.Raycast(ray, rayMaxDistance, InteractionObj))
+        {
+            OnMouseOver();
+        }
+        else
+        {
+            OnMouseExit();
+        }
     }
+
     public void OnMouseOver()
     {
-
+        image.sprite = selectedSprite;
     }
 
     public void OnMouseExit()
     {
-
+        image.sprite = idleSprite;
     }
 }
