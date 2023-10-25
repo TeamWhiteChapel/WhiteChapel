7eb70b527bab9cac4994dbde341f9585ebb75045
SShinMJ <winny0678@gmail.com>
2023-10-25 16:54:03 +0900

    Feat: 오브젝트 상세 탐색(상호작용) 기능 추가
    - 오브젝트 복제본을 카메라 근처에 활성화/비활성화
    - 플레이어 카메라 회전 입력 활성화/비활성화
    - 마우스 드래그에 따라 오브젝트 회전

--- WhiteChapel/Assets/1. Scripts/ObjectInterationUI/ItemSelectUI.cs
+++ WhiteChapel/Assets/1. Scripts/ObjectInterationUI/ItemSelectUI.cs
@@ -1,19 +1,34 @@
 using System;
-using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemSelectUI : MonoBehaviour
 {
+    [Tooltip("이미지를 띄울 UI Image")]
     [SerializeField] Image image;
+    [Tooltip("눈 감은 이미지")]
     [SerializeField] Sprite idleSprite;
+    [Tooltip("눈 뜬 이미지")]
     [SerializeField] Sprite selectedSprite;
 
     LayerMask InteractionObj;
+    [Tooltip("상호작용이 가능한 최대 거리")]
     [SerializeField] float rayMaxDistance = 0.5f;
 
+    [Tooltip("눈 이미지 UI")]
+    [SerializeField] GameObject InterationUI;
+    [Tooltip("오브젝트 상세 정보 UI")]
     [SerializeField] GameObject selectedUI;
+    [Tooltip("오브젝트 복제본(detailed)")]
+    [SerializeField] GameObject copyObj;
+    [Tooltip("player(GetPositionValue 스크립트를 가지는 오브젝트)")]
+    [SerializeField] GameObject player;
+    // 상호작용 중인 지(정보창 확인 중인지) 여부
     bool isSelectedUIActive;
+    [Tooltip("오브젝트와 카메라의 거리")]
+    [SerializeField] float distance = 0.4f;
+    [Tooltip("오브젝트가 왼쪽으로 xdistance만큼 이동")]
+    [SerializeField] float xdistance = -0.1f;
 
     private void Start()
     {
@@ -31,15 +46,17 @@ public class ItemSelectUI : MonoBehaviour
 
     void Update()
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-        RaycastHit hitInfo = new RaycastHit();
+        Ray inGameRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit inGameHit;
 
-        if (Physics.Raycast(ray, out hitInfo, rayMaxDistance, InteractionObj))
+        if (Physics.Raycast(inGameRay, out inGameHit, rayMaxDistance, InteractionObj))
         {
-            if(hitInfo.collider.gameObject == gameObject)
+            // 원본 오브젝트를 보고 있는 경우
+            if(inGameHit.collider.gameObject == gameObject)
             {
                 OnMouseOver();
 
+                // 상호작용이 가능한 오브젝트를 클릭/e키 하면 오브젝트 회전기능과 정보창이 보인다.
                 if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E))
                 {
                     OnObjectDetailedMode();
@@ -55,9 +72,11 @@ public class ItemSelectUI : MonoBehaviour
             OnMouseExit();
         }
 
+        // 오브젝트 정보창이 띄워져 있는 경우, 
         if (isSelectedUIActive)
         {
-            if(Input.GetKeyDown(KeyCode.Escape)) 
+            // ESC를 눌러 게임으로 돌아갈 수 있게 한다.
+            if (Input.GetKeyDown(KeyCode.Escape)) 
             {
                 OffObjectDetailedMode();
             }
@@ -69,9 +88,20 @@ public class ItemSelectUI : MonoBehaviour
         // ESC 키를 눌러 UI창 끄게하기 위한 용도.
         isSelectedUIActive = true;
 
-        // 카메라 오브젝트 회전으로 바꾸기
+        // 오브젝트 복제본을 카메라 앞으로 가져오고 방향을 바라보게한 후 활성화
+        Transform camera = Camera.main.transform;
+        copyObj.transform.position = camera.position + camera.forward * distance + camera.right * xdistance;
+        copyObj.transform.LookAt(copyObj.transform.position - Camera.main.transform.position);
 
-        // 오브젝트 데이터 불러와서 적용하기
+        // 마우스 입력값이 카메라 회전이 되지 않게 비활성화
+        player.GetComponent<GetPositionValue>().enabled = false;
+        Camera.main.GetComponent<CameraPC>().enabled = false;
+
+        // 상호작용 가능 표시 UI가 안보이게 하기 위해 OverUI 카메라 비활성화
+        Camera.main.transform.GetChild(0).gameObject.SetActive(false);
+
+        // 오브젝트 복제본 활성화
+        copyObj.SetActive(true);
 
         // 데이터 UI 활성화
         selectedUI.SetActive(true);
@@ -79,13 +109,21 @@ public class ItemSelectUI : MonoBehaviour
 
     private void OffObjectDetailedMode()
     {
-        // ESC 키를 눌러 UI창 끄게하기 위한 용도.
-        isSelectedUIActive = false;
-        
-        // 카메라 플레이어 회전으로 바꾸기
+        // 오브젝트 복제본 비활성화
+        copyObj.SetActive(false);
 
         // 데이터 UI 비활성화
         selectedUI.SetActive(false);
+
+        // 상호작용 가능 표시 UI가 보이게 하기 위해 OverUI 카메라 활성화
+        Camera.main.transform.GetChild(0).gameObject.SetActive(true);
+
+        // 마우스 입력값이 카메라 회전이 되게 활성화
+        player.GetComponent<GetPositionValue>().enabled = true;
+        Camera.main.GetComponent<CameraPC>().enabled = true;
+
+        // ESC 키를 눌러 UI창 끄게하기 위한 용도.
+        isSelectedUIActive = false;
     }
 
     public void OnMouseOver()
